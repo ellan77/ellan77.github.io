21af1bf62bbcd099dd1eb71e983153fb9b0d44be
Dmitry Smyrnov <smyrnovd@gmail.com>
2020-02-26 15:37:13 +0200

    rebuilding site Wed Feb 26 15:37:12 EET 2020

---
+++ webacademy/hw2/precache-manifest.0ae84044685e3e9a58fec6cd8dcb0c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8fee398a0e264de531a3663cd25115",
+    "url": "/webacademy/hw2/index.html"
+  },
+  {
+    "revision": "83cf269d31115fd1369d",
+    "url": "/webacademy/hw2/static/css/main.5f9584b6.chunk.css"
+  },
+  {
+    "revision": "8378e5be93156efdc591",
+    "url": "/webacademy/hw2/static/js/2.f9494237.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/webacademy/hw2/static/js/2.f9494237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83cf269d31115fd1369d",
+    "url": "/webacademy/hw2/static/js/main.151935ee.chunk.js"
+  },
+  {
+    "revision": "f669ae48eaf5235000d4",
+    "url": "/webacademy/hw2/static/js/runtime-main.0b6a82b0.js"
+  }
+]);
